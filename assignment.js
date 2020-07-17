3eb8930eab77619a35b6b5fe5c8d2c0bab52be94
tanvir5230 <tanvirbintouhid5230@gmail.com>
2020-07-17 10:33:30 +0600

    project initialised

---
+++ assignment.js
@@ -0,0 +1,19 @@
+// assignment-1
+function feetToMile(feet) {
+  return mile;
+}
+
+// assignment-2
+function woodCalculator(table, chair, cot) {
+  return totalWood;
+}
+
+// assignment-3
+function brickCalculator(noOfFloor) {
+  return totalBrickNumber;
+}
+
+// assignment-4
+function tinyFriend(arrayOfFriendsName) {
+  return tiny;
+}
